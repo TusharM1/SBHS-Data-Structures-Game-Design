ee2d420d0010630ed0f0e8645769b19345b1c17e
TusharM1 <tman08810@gmail.com>
2019-01-19 12:47:52 -0500

    HashSet

---
+++ src/Quarter_2/RandomIntegers.java
@@ -0,0 +1,32 @@
+package Quarter_2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class RandomIntegers {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        File name = new File("src/Quarter_2/random_integers.txt");
+        try {
+            BufferedReader input = new BufferedReader(new FileReader(name));
+            String text;
+            while ((text = input.readLine()) != null) {
+                int number = Integer.parseInt(text);
+                if (hashMap.containsKey(number))
+                    hashMap.put(number, hashMap.get(number) + 1);
+                else
+                    hashMap.put(number, 1);
+            }
+            System.out.println(hashMap);
+        }
+        catch (IOException io) {
+            System.err.println("File does not exist");
+        }
+    }
+
+}
